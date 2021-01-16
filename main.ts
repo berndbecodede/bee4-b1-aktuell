2c3cc84414c767ca249ed18ed09fb5ad170bbc3e
berndbecodede <42220926+berndbecodede@users.noreply.github.com>
2021-01-16 01:15:14 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-input.onPinPressed(TouchPin.P0, function () {
-    basic.showCompass(5000)
-})
 input.onPinPressed(TouchPin.P3, function () {
     basic.showString("3")
     basic.showIcon(IconNames.SmallSquare)
@@ -11,7 +8,6 @@ input.onPinPressed(TouchPin.P3, function () {
         . . . . .
         . . . . .
         `).showImage(0)
-    basic.showString("a-b")
     images.createImage(`
         # . . . .
         . # # # .
@@ -20,7 +16,6 @@ input.onPinPressed(TouchPin.P3, function () {
         . . . . .
         `).showImage(0)
     while (input.buttonIsPressed(Button.B)) {
-        basic.setLedColor(0x00ff00)
         images.createImage(`
             # . . . .
             . . . . .
@@ -28,7 +23,6 @@ input.onPinPressed(TouchPin.P3, function () {
             . . . . .
             . . . . #
             `).showImage(0)
-        basic.setLedColor(Colors.Indigo)
         images.createImage(`
             # . . . .
             . # . . .
@@ -50,7 +44,6 @@ input.onPinPressed(TouchPin.P3, function () {
             . . . # .
             . . . . #
             `).showImage(0)
-        basic.setLedColor(Colors.Purple)
         images.createImage(`
             # . . . .
             . . . . .
@@ -72,14 +65,6 @@ input.onPinPressed(TouchPin.P3, function () {
             . . . . .
             . . . . #
             `).showImage(0)
-        basic.setLedColor(Colors.Off)
-        images.createImage(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `).showImage(0)
         images.createImage(`
             . . . . #
             . . . . .
@@ -87,7 +72,6 @@ input.onPinPressed(TouchPin.P3, function () {
             . . . . .
             . . . . .
             `).showImage(0)
-        music.playTone(392, music.beat(BeatFraction.Quarter))
         images.createImage(`
             . . . . #
             . . . . .
@@ -130,7 +114,6 @@ input.onPinPressed(TouchPin.P3, function () {
             # . # # #
             # # # # #
             `).showImage(0)
-        music.playTone(494, music.beat(BeatFraction.Quarter))
         images.createImage(`
             # # # # .
             # # # # #
@@ -180,7 +163,6 @@ input.onPinPressed(TouchPin.P3, function () {
             # # # # #
             # # # # #
             `).showImage(0)
-        music.playTone(262, music.beat(BeatFraction.Quarter))
         images.createImage(`
             . . . . .
             . . . . .
@@ -216,203 +198,11 @@ input.onPinPressed(TouchPin.P3, function () {
             . # . . .
             . # . . #
             `).showImage(0)
-        music.playTone(262, music.beat(BeatFraction.Quarter))
         images.createImage(`
             . . . . .
             . . . . .
             . . . . .
             . . . # .
-            . . . . .
-            `).showImage(0)
-        basic.setLedColor(0xff0000)
-        images.createImage(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `).showImage(0)
-        basic.turnRgbLedOff()
-        basic.clearScreen()
-    }
-    while (input.buttonIsPressed(Button.A)) {
-        basic.setLedColor(0xff8000)
-        images.createImage(`
-            . . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . .
-            `).showImage(0)
-        basic.setLedColor(Colors.Green)
-        images.createImage(`
-            . . . . #
-            . . . # .
-            . . . . .
-            . # . . .
-            # . . . .
-            `).showImage(0)
-        music.playTone(659, music.beat(BeatFraction.Quarter))
-        images.createImage(`
-            . . . . #
-            . . . # .
-            . . # . .
-            . # . . .
-            # . . . .
-            `).showImage(0)
-        basic.setLedColor(Colors.Violet)
-        images.createImage(`
-            . . . . #
-            . . . # .
-            . . . . .
-            . # . . .
-            # . . . .
-            `).showImage(0)
-        music.playTone(165, music.beat(BeatFraction.Quarter))
-        images.createImage(`
-            . . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . .
-            `).showImage(0)
-        images.createImage(`
-            # . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `).showImage(0)
-        basic.setLedColor(Colors.Yellow)
-        images.createImage(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `).showImage(0)
-        images.createImage(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . .
-            `).showImage(0)
-        music.playTone(262, music.beat(BeatFraction.Quarter))
-        images.createImage(`
-            # . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . .
-            `).showImage(0)
-        images.createImage(`
-            # . . . .
-            # . . . .
-            # . . . .
-            . . . . .
-            # . . . .
-            `).showImage(0)
-        images.createImage(`
-            . . . . .
-            # . . . .
-            # . . . .
-            # . . . .
-            # . . . .
-            `).showImage(0)
-        music.playTone(784, music.beat(BeatFraction.Quarter))
-        images.createImage(`
-            . # # . .
-            . # # . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `).showImage(0)
-        images.createImage(`
-            . . . . .
-            # . . . .
-            # . . . .
-            # # # . .
-            # # # . .
-            `).showImage(0)
-        basic.setLedColor(0x00ff00)
-        basic.pause(100)
-        images.createImage(`
-            . # # # .
-            # . . # .
-            # . . . .
-            # # # . .
-            # # # . .
-            `).showImage(0)
-        music.playTone(494, music.beat(BeatFraction.Quarter))
-        images.createImage(`
-            . . . . .
-            # # # # .
-            # . . # .
-            # # # . .
-            # # # . .
-            `).showImage(0)
-        basic.setLedColor(0xff8000)
-        basic.pause(100)
-        images.createImage(`
-            . . . . .
-            # . . . .
-            # # # # .
-            # # # # .
-            # # # . .
-            `).showImage(0)
-        basic.turnRgbLedOff()
-        images.createImage(`
-            . . . . .
-            # . . . .
-            # . . . .
-            # # # # .
-            # # # # .
-            `).showImage(0)
-        images.createImage(`
-            . . . . .
-            # . . . .
-            # # # . .
-            # # # # .
-            # # # # .
-            `).showImage(0)
-        images.createImage(`
-            . . . . .
-            # . # # #
-            # # . # #
-            # # # . #
-            # # # # .
-            `).showImage(0)
-        images.createImage(`
-            . # # # #
-            # . # # #
-            # # . # #
-            # # # . #
-            # # # # .
-            `).showImage(0)
-        images.createImage(`
-            # # # # #
-            . # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `).showImage(0)
-        music.playTone(784, music.beat(BeatFraction.Quarter))
-        images.createImage(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `).showImage(0)
-        basic.setLedColor(0xff0000)
-        basic.showString("ERROR")
-        basic.turnRgbLedOff()
-        images.createImage(`
-            . . . . .
-            . . . . .
-            # . . . .
-            . . . . .
             . . . . .
             `).showImage(0)
         images.createImage(`
@@ -422,44 +212,6 @@ input.onPinPressed(TouchPin.P3, function () {
             . . . . .
             . . . . .
             `).showImage(0)
-        images.createImage(`
-            . . . . .
-            . . . . .
-            . . . . #
-            . . . . .
-            . . . . .
-            `).showImage(0)
-        images.createImage(`
-            # . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `).showImage(0)
-        images.createImage(`
-            . # . . .
-            # # # # #
-            . # . . .
-            . # . . .
-            . # . . #
-            `).showImage(0)
-        music.playTone(262, music.beat(BeatFraction.Quarter))
-        images.createImage(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . # .
-            . . . . .
-            `).showImage(0)
-        basic.setLedColor(0xff0000)
-        images.createImage(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `).showImage(0)
-        basic.turnRgbLedOff()
         basic.clearScreen()
     }
 })
@@ -715,9 +467,6 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Tortoise)
     basic.clearScreen()
 })
-input.onPinPressed(TouchPin.P2, function () {
-    basic.showString("2")
-})
 input.onGesture(Gesture.Shake, function () {
     control.reset()
 })
@@ -736,11 +485,6 @@ input.onButtonPressed(Button.B, function () {
         basic.setLedColor(Colors.Off)
         basic.clearScreen()
     }
-})
-input.onPinPressed(TouchPin.P1, function () {
-    basic.showString("p1")
-    basic.pause(100)
-    basic.showIcon(IconNames.Heart)
 })
 basic.clearScreen()
 basic.setLedColor(0x00ff00)
